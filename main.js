c6ed1688dcf882c69ecfa1cc4d16484d2ff482cb
Cristian-Marcano <cristianmm.1112@gmail.com>
2025-02-07 15:34:31 -0400

    refactor(mobile_menu) & fix(translateButton)

--- main.js
+++ main.js
@@ -3,10 +3,8 @@
 const content = document.querySelectorAll('main > section');
 const navMobile = document.querySelectorAll('.mobile > a');
 const navDesktop = document.querySelectorAll('.desktop ul li');
-const buttonMenu = document.querySelector('.mobile__menu > button');
-const menuMobile = document.querySelector('.menu');
 const themeDOM = document.querySelector(':root');
-const translateButton = document.querySelectorAll('ul li a[title*="Translate"]');
+const translateButton = document.querySelectorAll('a[title*="Translate"]');
 
 window.onscroll = () => {
     content.forEach(sec => {
@@ -31,22 +29,6 @@ const getLanguagePreference = () => {
 
     return 'es';
 }
-
-const toggleMenu = (e) => {
-    e.stopPropagation();
-    buttonMenu.firstElementChild.classList.toggle('selected');
-    buttonMenu.lastElementChild.classList.toggle('selected');
-    menuMobile.classList.toggle('hidden');
-}
-
-const hideMenu = (e) => {
-    buttonMenu.firstElementChild.classList.add('selected');
-    buttonMenu.lastElementChild.classList.remove('selected');
-    menuMobile.classList.add('hidden');
-}
-
-buttonMenu.addEventListener('click',toggleMenu);
-document.addEventListener('click', hideMenu);
 
 let language = getLanguagePreference();
 document.documentElement.setAttribute('lang', language);
